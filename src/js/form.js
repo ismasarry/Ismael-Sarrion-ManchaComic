3c5b8bb7c6b528fbff07ada6e79af2395e320ee9
Ismael <145430109+ismasarry@users.noreply.github.com>
2024-10-14 09:26:02 +0200

    añado el apartado del formulario de control de las horas y elimino el control de errores por alertas

--- src/js/form.js
+++ src/js/form.js
@@ -1,8 +1,10 @@
-
 document.addEventListener('DOMContentLoaded', function () {
     var form = document.getElementById('formularioActividad');
     var tipoActividad = document.getElementById('tipoActividad');
     var sitioActividad = document.getElementById('sitioActividad');
+    var diaActividad = document.getElementById('diaActividad');
+    var horaActividad = document.getElementById('horaActividad');
+    var errorMessage = document.getElementById('error-message');
 
     var sitiosOriginales = [
         "Jardín del Prado",
@@ -37,25 +39,43 @@ document.addEventListener('DOMContentLoaded', function () {
                 sitioActividad.appendChild(option);
             });
         }
-        
     }
 
+    function actualizarHoras() {
+        var diaSeleccionado = diaActividad.value;
+        horaActividad.innerHTML = '<option value="">Seleccione una hora</option>'; // Resetea las opciones de hora
+
+        let horasDisponibles;
+
+        if (diaSeleccionado === "Viernes") {
+            horasDisponibles = ["17h", "18h", "19h", "20h", "21h", "22h"];
+        } else if (diaSeleccionado === "Sábado" || diaSeleccionado === "Domingo") {
+            horasDisponibles = ["10h", "11h", "12h", "13h", "14h", "15h", "16h", "17h", "18h", "19h", "20h", "21h", "22h"];
+        }
+
+        if (horasDisponibles) {
+            horasDisponibles.forEach(function(hora) {
+                var option = document.createElement('option');
+                option.value = hora;
+                option.text = hora;
+                horaActividad.appendChild(option);
+            });
+        }
+    }
+
+    diaActividad.addEventListener('change', actualizarHoras);
     tipoActividad.addEventListener('change', cambiarSitioActividad);
 
     form.addEventListener('submit', function (event) {
         event.preventDefault();
-        event.stopPropagation();
+        errorMessage.style.display = 'none'; 
+        errorMessage.textContent = ''; 
 
         if (form.checkValidity() === false) {
-            form.classList.add('es correcto');
+            errorMessage.textContent = 'Por favor, complete todos los campos requeridos correctamente.';
+            errorMessage.style.display = 'block';
         } else {
-         
-            alert('Actividad registrada exitosamente.');
-            form.reset();
-            form.classList.remove('es correcto');
-            cambiarSitioActividad();
+            window.location.href = '../public/schedule.html'; 
         }
     }, false);
-
-   
 });
