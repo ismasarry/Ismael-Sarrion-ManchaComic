980b0e9e330ab3683cd7611c28af041388ec1972
Ismael <145430109+ismasarry@users.noreply.github.com>
2024-10-17 08:28:03 +0200

    guardo los valores del formulario en el localStorage y los manejo en schedule.js

---
+++ src/js/schedule.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var tabla = document.getElementById('tablaActividades');
+    
+    var actividades = JSON.parse(localStorage.getItem('actividades')) || [];
+
+});
